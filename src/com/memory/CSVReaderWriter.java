9c3710716bdf8dfbb3c2ec6bb23ad2c24953a7c6
Enrico Bruno Del Zotto <enrico.delzotto@tuitravel.com>
2018-02-13 18:17:24 +0000

    * Review / comments

--- src/com/memory/CSVReaderWriter.java
+++ src/com/memory/CSVReaderWriter.java
@@ -1,25 +1,36 @@
 package com.memory;
 
-/*
-A developer was tasked with writing a reusable implementation for a mass mailing application to read and write text files that hold tab separated data. He proceeded and as a result produced the CSVReaderWriter class.
-
-His implementation, although it works and meets the needs of the application, is of very low quality.
-
-Your task:
-     - Identify and annotate the shortcomings in the current implementation as if you were doing a code review, using comments in the CSVReaderWriter.java file.
-     - Refactor the CSVReaderWriter implementation into clean, elegant, rock-solid & well performing code, without over-engineering.
-     - Where you make trade offs, comment & explain.
-     - Assume this code is in production and backwards compatibility must be maintained. Therefore if you decide to change the public interface,
-       please deprecate the existing methods. Feel free to evolve the code in other ways though.
-     - You can add modules and jars as you see fit as long as everything will build on our systems without manual intervention.
-*/
+/**
+ * Your task:
+ * Identify and annotate the shortcomings in the current implementation as if you were doing a
+ * code review, using comments in the CSVReaderWriter.java file.
+ *
+ * General comments :
+ * 
+ * 1- Naming conventions : I really don't like the C# style of the code, i know in android there
+ *    is not a very clear naming convention, but this is too far away from the google style convetions.
+ *
+ * 2- This class come without tests, TDD is always a good mode 'how to write code'
+ *
+ * 3- This class violates the Single Responsibility Principle (Read and Write in same class)
+ *    and also the Don't Repeat Yourself principle (for example the read method has in common lot of code)
+ *    I would suggest to break the responsibility of reading and writing to separate classes which could also be derived
+ *    from some interface.
+ *
+ * 4- The class is doing I/O in a synchronous fashion, which is a terrible idea.
+ *    Using multithreading in this case can improve the performances and the most dangerous thing I/O methods are not thread safe.
+ * */
 
 import java.io.*;
 
 public class CSVReaderWriter {
+    //Naming convention, why c# ?
     private BufferedReader _bufferedReader = null;
     private BufferedWriter _bufferedWriter = null;
 
+    // It's any particular reason why we are associate to an ENUM to an int here ?
+    // We have only to mode (read and write) so we don't need to do it !
+    // Also the mode should be capital, so the name convention is not good also here
     public enum Mode {
         Read (1), Write(2);
 
@@ -33,43 +44,46 @@ public class CSVReaderWriter {
     }
 
     public void open(String fileName, Mode mode) throws Exception {
-        if (mode == Mode.Read)
-        {
+        // We should check parameters. For example, whether file name is valid.
+        if (mode == Mode.Read) {
             _bufferedReader = new BufferedReader(new FileReader(fileName));
-        }
-        else if (mode == Mode.Write)
-        {
+        } else if (mode == Mode.Write) {
             FileWriter fileWriter = new FileWriter(fileName);
             _bufferedWriter = new BufferedWriter(fileWriter);
-        }
-        else
-        {
+        } else {
+            //Why general exception ? Better throw always the correct one
             throw new Exception("Unknown file mode for " + fileName);
         }
     }
 
+    //Why this method is not thread safe at all ?
     public void write(String... columns) throws IOException {
         String outPut = "";
-
+        // There is no point in inventing such a thing. You can just use string.Join.
+        // The less new code, the less space for mistakes.
         for (int i = 0; i < columns.length; i++)
         {
             outPut += columns[i];
             if ((columns.length - 1) != i)
             {
+                //Any reason why this char could not be static or passed into the constructor ?
                 outPut += "\t";
             }
         }
 
         writeLine(outPut);
     }
-
+    //Why this method is not thread safe at all ?
     public boolean read(String[] columns) throws IOException {
+        // If you were to use these constants and had two places where you need them
+        // then you should make them global class static final constant
         final int FIRST_COLUMN = 0;
         final int SECOND_COLUMN = 1;
 
         String line;
         String[] splitLine;
 
+        //Any reason why this char could not be declared as global static variable or passed into the constructor ?
         String separator = "\t";
 
         line = readLine();
@@ -90,14 +104,17 @@ public class CSVReaderWriter {
             return true;
         }
     }
-
+    //Why this methos is not thread safe at all ?
     public boolean read(String column1, String column2) throws IOException {
+        // If you were to use these constants and had two places where you need them
+        // then you should make them global class static final constant
         final int FIRST_COLUMN = 0;
         final int SECOND_COLUMN = 1;
-
+        //This method can be extracted and used in the first read
         String line;
         String[] splitLine;
 
+        //Any reason why this char could not be declared as global static variable or passed into the constructor ?
         String separator = "\t";
 
         line = readLine();
@@ -119,6 +136,7 @@ public class CSVReaderWriter {
 
             return false;
         }
+        // No check, that column length is at least size of 2
         else
         {
             column1 = splitLine[FIRST_COLUMN];
@@ -128,10 +146,11 @@ public class CSVReaderWriter {
         }
     }
 
+    //Why this method is not thread safe at all ?
     private void writeLine(String line) throws IOException {
         _bufferedWriter.write(line);
     }
-
+    //Why this method is not thread safe at all ?
     private String readLine() throws IOException {
         return _bufferedReader.readLine();
     }
