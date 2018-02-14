e0ccce3e6c95a1cdac62fb8001d9aa5a8c3276dd
Enrico Bruno Del Zotto <enrico.delzotto@tuitravel.com>
2018-02-14 09:48:34 +0000

    * Fixing comments

--- src/com/memory/CSVReaderWriter.java
+++ src/com/memory/CSVReaderWriter.java
@@ -4,22 +4,24 @@ package com.memory;
  * Your task:
  * Identify and annotate the shortcomings in the current implementation as if you were doing a
  * code review, using comments in the CSVReaderWriter.java file.
- *
+ * <p>
  * General comments :
- * 
- * 1- Naming conventions : I really don't like the C# style of the code, i know in android there
- *    is not a very clear naming convention, but this is too far away from the google style convetions.
- *
+ * <p>
+ * 1- Read and write are not thread safe
+ * <p>
  * 2- This class come without tests, TDD is always a good mode 'how to write code'
- *
+ * <p>
  * 3- This class violates the Single Responsibility Principle (Read and Write in same class)
- *    and also the Don't Repeat Yourself principle (for example the read method has in common lot of code)
- *    I would suggest to break the responsibility of reading and writing to separate classes which could also be derived
- *    from some interface.
- *
+ * and also the Don't Repeat Yourself principle (for example the read method has in common lot of code)
+ * I would suggest to break the responsibility of reading and writing to separate classes which could also be derived
+ * from some interface.
+ * <p>
  * 4- The class is doing I/O in a synchronous fashion, which is a terrible idea.
- *    Using multithreading in this case can improve the performances and the most dangerous thing I/O methods are not thread safe.
- * */
+ * Using multithreading in this case can improve the performances and the most dangerous thing I/O methods are not thread safe.
+ * <p>
+ * 5- Naming conventions : I really don't like the C# style of the code, i know in Android there
+ * is not a very clear naming convention, but this is too far away from the google style.
+ */
 
 import java.io.*;
 
@@ -30,14 +32,16 @@ public class CSVReaderWriter {
 
     // It's any particular reason why we are associate to an ENUM to an int here ?
     // We have only to mode (read and write) so we don't need to do it !
-    // Also the mode should be capital, so the name convention is not good also here
+    // Also the mode name should be capital
     public enum Mode {
-        Read (1), Write(2);
+        Read(1), Write(2);
 
         private int _mode;
+
         Mode(int mode) {
             this._mode = mode;
         }
+
         public int getMode() {
             return _mode;
         }
@@ -61,11 +65,9 @@ public class CSVReaderWriter {
         String outPut = "";
         // There is no point in inventing such a thing. You can just use string.Join.
         // The less new code, the less space for mistakes.
-        for (int i = 0; i < columns.length; i++)
-        {
+        for (int i = 0; i < columns.length; i++) {
             outPut += columns[i];
-            if ((columns.length - 1) != i)
-            {
+            if ((columns.length - 1) != i) {
                 //Any reason why this char could not be static or passed into the constructor ?
                 outPut += "\t";
             }
@@ -73,6 +75,7 @@ public class CSVReaderWriter {
 
         writeLine(outPut);
     }
+
     //Why this method is not thread safe at all ?
     public boolean read(String[] columns) throws IOException {
         // If you were to use these constants and had two places where you need them
@@ -89,22 +92,21 @@ public class CSVReaderWriter {
         line = readLine();
         splitLine = line.split(separator);
 
-        if (splitLine.length == 0)
-        {
+        if (splitLine.length == 0) {
             columns[0] = null;
             columns[1] = null;
 
             return false;
-        }
-        else
-        {
+        } else {
             columns[0] = splitLine[FIRST_COLUMN];
             columns[1] = splitLine[SECOND_COLUMN];
 
             return true;
         }
     }
-    //Why this methos is not thread safe at all ?
+
+    //Why this method is not thread safe at all ?
+    //Internally this method is same of the one above.
     public boolean read(String column1, String column2) throws IOException {
         // If you were to use these constants and had two places where you need them
         // then you should make them global class static final constant
@@ -119,8 +121,7 @@ public class CSVReaderWriter {
 
         line = readLine();
 
-        if (line == null)
-        {
+        if (line == null) {
             column1 = null;
             column2 = null;
 
@@ -129,16 +130,14 @@ public class CSVReaderWriter {
 
         splitLine = line.split(separator);
 
-        if (splitLine.length == 0)
-        {
+        if (splitLine.length == 0) {
             column1 = null;
             column2 = null;
 
             return false;
         }
         // No check, that column length is at least size of 2
-        else
-        {
+        else {
             column1 = splitLine[FIRST_COLUMN];
             column2 = splitLine[SECOND_COLUMN];
 
@@ -147,22 +146,25 @@ public class CSVReaderWriter {
     }
 
     //Why this method is not thread safe at all ?
+    //Are we assume after write line we always reach the max of the buffer and it is flushed ?
+    //Or we always close after write ?
     private void writeLine(String line) throws IOException {
         _bufferedWriter.write(line);
     }
+
     //Why this method is not thread safe at all ?
     private String readLine() throws IOException {
         return _bufferedReader.readLine();
     }
 
+    //Why we are closing two times the buffered writer ?
+    //and we are not closing the _bufferedReader?
     public void close() throws IOException {
-        if (_bufferedWriter != null)
-        {
+        if (_bufferedWriter != null) {
             _bufferedWriter.close();
         }
 
-        if (_bufferedWriter != null)
-        {
+        if (_bufferedWriter != null) {
             _bufferedWriter.close();
         }
     }
